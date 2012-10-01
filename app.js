d730e159623d264a64c355486231a5996ac8034d
Dan North <dan@dannorth.net>
2012-10-01 15:47:37 +0100

    [dn] Retrieve data from server

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ var express = require('express');
 app = express();
 app.use(express.logger());
 app.use(express.static('static'));
+
+app.get('/summaries', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify([
+        { url: 'http://stylecouncil.org', description: 'Cafe Bleu' },
+        { url: 'http://another.com', description: 'Cafe Cafe' },
+    ]));
+});
+
 app.listen(8080);
